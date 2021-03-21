1932e0ae5915cb4bae344ff5825a4e711ddcb092
martin.boissier <martin.boissier@hpi.de>
2021-03-21 13:39:54 +0100

    Working

--- test_pipeline.sh
+++ test_pipeline.sh
@@ -1,10 +1,12 @@
 #!/bin/sh -l
 
-git clone --recursive https://github.com/hyrise/hyrise.git
+git clone --recursive https://github.com/Bouncner/hyrise_workload_analysis_plugin.git
 cd hyrise
 
 ./install_dependencies.sh
 apt-get install psql
+
+cd ..
 
 mkdir clang-debug && cd clang-debug && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ ..
 make WorkloadStatistics SimpleExecutor hyriseServer
